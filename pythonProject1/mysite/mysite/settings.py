b90bfba29860fd9ad4b53c6d150ba69b502f58e7
carla58-58 <carlaa_58@hotmail.com>
2025-08-04 21:24:17 +1000

    settings.py changes

--- pythonProject1/mysite/mysite/settings.py
+++ pythonProject1/mysite/mysite/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'librarymanagementdb',
+        'USER': 'postgres',
+        'PASSWORD': 'librarymanagement',
+        'HOST': 'librarymanagementdb.cwhg400qkyug.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
